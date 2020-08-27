91fd2abf89c227dd4dfb25511f2caf7c569bbf6c
shengchaojie <shengchaojie@163.com>
2020-08-27 14:02:25 +0800

    通用请求接口问题修复

--- src/main/java/com/dsc/financeopen/request/CustomOpenRequestParam.java
+++ src/main/java/com/dsc/financeopen/request/CustomOpenRequestParam.java
@@ -6,7 +6,7 @@ import java.util.Map;
  * @author shengchaojie
  * @date 2020/7/30
  **/
-public class CustomOpenRequestParam extends OpenRequestParam<String,Map<String,String>>{
+public class CustomOpenRequestParam extends OpenRequestParam<String,Map<String,Object>>{
     @Override
     public Class<String> getResultClass() {
         return String.class;
